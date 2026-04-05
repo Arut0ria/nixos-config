34d26b1d5fb2547483c85e6063450dfd1f7fd4e9
Arut0ria <anoxing64@gmail.com>
2026-04-05 18:58:10 +0200

    (Fix) Adding firewall,interface and port support to minidlna module.

--- modules/services/dlna.nix
+++ modules/services/dlna.nix
@@ -19,17 +19,34 @@ in
         ];
         description = "List of directory visible by mini-dlna service (see doc).";
       };
+
+      networkInterface = lib.mkOption {
+        type = lib.types.str;
+        default = "wlo1";
+        description = "The interface to listen to.";
+      };
+
+      port = lib.mkOption {
+        type = lib.types.int;
+        default = 8200;
+        description = "The port to listen to";
+      };
     };
   };
 
   config = lib.mkIf config.dlna-module.enable {
-    services.minidlna.enable = true;
-    services.minidlna.settings = {
-      friendly_name = cfg.settings.name;
-      media_dir = cfg.settings.mediaDir;
-      log_level = "error";
-      inotify = "yes";
-      wide_links = "yes";
+    services.minidlna = {
+      enable = true;
+      settings = {
+        friendly_name = cfg.settings.name;
+        media_dir = cfg.settings.mediaDir;
+        log_level = "warning";
+        inotify = "yes";
+        wide_links = "yes";
+        network_interface = cfg.settings.networkInterface;
+        port = cfg.settings.port;
+        openFirewall = true;
+      };
     };
 
     users.users.minidlna = {
