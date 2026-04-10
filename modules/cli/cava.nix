fb5b83a58855a676beb6f4fa6e406254f150cef5
Arut0ria <anoxing64@gmail.com>
2026-04-10 14:18:05 +0200

    (Refactor) Rewrote cli modules to better work with flake-parts.

--- modules/cli/cava.nix
+++ modules/cli/cava.nix
@@ -1,13 +1,17 @@
-{ config, lib, pkgs, ... }: {
-  options = {
-    cava-module.enable = lib.mkEnableOption "Enables cava.";
-  };
-
-  config = lib.mkIf config.cava-module.enable {
-    # stylix.targets.cava.rainbow.enable = lib.mkIf (builtins.hasAttr "stylix" args.inputs) true;
-
-    programs.cava = {
-      enable = true;
+{
+  ...
+}:
+{
+  flake.homeModules.cava =
+    {
+      ...
+    }:
+    {
+      config = {
+        # stylix.targets.cava.rainbow.enable = lib.mkIf (builtins.hasAttr "stylix" args.inputs) true;
+        programs.cava = {
+          enable = true;
+        };
+      };
     };
-  };
 }
--- modules/cli/ranger.nix
+++ modules/cli/ranger.nix
@@ -1,14 +1,20 @@
-{ pkgs, config, lib, ... }: {
-  options = {
-    ranger-module.enable = lib.mkEnableOption "Enables Ranger";
-  };
-
-  config = lib.mkIf config.ranger-module.enable {
-    programs.ranger = {
-      enable = true;
-      settings = {
-        preview_images_method = lib.optionals (config.kitty-module.enable) "kitty";
+{
+  ...
+}:
+{
+  flake.homeModules.ranger =
+    {
+      lib,
+      ...
+    }:
+    {
+      config = {
+        programs.ranger = {
+          enable = true;
+          settings = {
+            preview_images_method = lib.mkDefault "kitty";
+          };
+        };
       };
     };
-  };
 }
